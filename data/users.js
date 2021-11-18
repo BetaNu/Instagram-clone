ed77fe664a09861571010cbf3e87dd019821bb43
BetaNu <ben.dubourguier@gmail.com>
2021-11-18 14:35:18 +0000

    Stories component with Users in raw data

---
+++ data/users.js
@@ -0,0 +1,26 @@
+export const USERS = [
+    {
+        user: 'DFSBoy',
+        image: 'https://randomuser.me/api/portraits/men/62.jpg'
+    },
+    {
+        user: 'ChiefThreeStars',
+        image: 'https://randomuser.me/api/portraits/lego/8.jpg'
+    },
+    {
+        user: 'Triss',
+        image: 'https://randomuser.me/api/portraits/women/66.jpg'
+    },
+    {
+        user: 'Bob',
+        image: 'https://randomuser.me/api/portraits/men/15.jpg'
+    },
+    {
+        user: 'Coct56',
+        image: 'https://randomuser.me/api/portraits/women/17.jpg'
+    },
+    {
+        user: 'CleverJulius',
+        image: 'https://randomuser.me/api/portraits/men/55.jpg'
+    },
+]
